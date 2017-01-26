92968d0ee18c8e12fcbd2044a35725eadbdde52c
Hemant Sanvaria <hsanvaria@ccistudios.com>
2017-01-26 13:55:36 -0500

    script and css version

--- golf-registration.php
+++ golf-registration.php
@@ -6,14 +6,14 @@
     <meta charset="UTF-8">
     <title>Fighting Irish Legacy Club | St. Patrick's Fighting Irish | Golf Registration Form</title>
     <link rel="icon" type="image/png" href="images/favicon.png">
-    <link rel="stylesheet" type="text/css" href="css/style.css?v1='133'" media="all">
+    <link rel="stylesheet" type="text/css" href="css/style.css?v1='140'" media="all">
     <script type="text/javascript" src="https://js.stripe.com/v2"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script type="text/javascript">
         Stripe.setPublishableKey('<?php echo $config['publishable-key'] ?>');
     </script>
     <script type="text/javascript" src="golf.js"></script>      
-    <script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript" src="script.js?v1='100'"></script>
 </head>
 <body>
     <div id="wrapper">
