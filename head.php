7117bdd9821cbcd11385b97721924797617f3a41
Chris Morris <cmorris@ccistudios.com>
2017-02-13 17:00:21 -0500

    increase width of the province dropdown on donation form

--- head.php
+++ head.php
@@ -9,7 +9,7 @@ $config = require(__DIR__.'/config.php');
     <link rel="icon" type="image/png" href="/images/favicon.png">
     <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,800,800i" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="/css/style.css?v=5" media="all">
+    <link rel="stylesheet" type="text/css" href="/css/style.css?v=6" media="all">
     <script type="text/javascript" src="/js/jquery-1.11.0.min.js"></script>
 </head>
 <body>
